28038c9ecc5a5c32ff4035c6c8d342973a70d2fa
francisco5em <francisco5em@hotmail.com>
2023-12-29 00:56:31 -0700

    Initial commit

---
+++ src/test/java/com/francisco5em/springrestmvc/SpringRestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco5em.springrestmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
